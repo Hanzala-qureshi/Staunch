59f3d87e111636370390487c2a3475682a355679
Faisalshk <faisalshaikh5656@gmail.com>
2024-09-04 17:31:56 +0530

    hero section updates as per requirement, Font added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
 import { ServicesComponent } from './services/services.component';
 import { IndustriesComponent } from './industries/industries.component';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,7 @@ import { IndustriesComponent } from './industries/industries.component';
     HeroComponent,
     ServicesComponent,
     IndustriesComponent,
+    SlickCarouselModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,12 +1,84 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
+  slides = [
+    {
+      title: 'Staffing and Resource Augmentation',
+      description: 'Our values define Staunch Consulting Solution...',
+      backgroundImage: '../../assets/hero1.jpg',
+    },
+    {
+      title: 'Support and Maintenance',
+      description: 'We guide you in delivering excellence...',
+      backgroundImage: '../../assets/hero2.jpg',
+    },
+    {
+      title: 'Quality Assurance and Testing',
+      description: 'Trust in every IT solution...',
+      backgroundImage: '../../assets/hero3.jpg',
+    },
+    {
+      title: 'Software Development',
+      description: 'Trust in every IT solution...',
+      backgroundImage: '../../assets/hero4.jpg',
+    },
+    {
+      title: 'Industry 4.0 Services',
+      description: 'Trust in every IT solution...',
+      backgroundImage: '../../assets/hero5.jpg',
+    },
+    {
+      title: 'Strategic Consulting and Advisory',
+      description: 'Trust in every IT solution...',
+      backgroundImage: '../../assets/hero6.jpg',
+    },
+    {
+      title: 'Agile Transformation Consulting',
+      description: 'Trust in every IT solution...',
+      backgroundImage: '../../assets/hero7.jpg',
+    },
+    {
+      title: 'Business Processing Outsourcing',
+      description: 'Trust in every IT solution...',
+      backgroundImage: '../../assets/hero8.jpg',
+    },
+  ];
 
+  currentSlide = 0;
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide() {
+    setInterval(() => {
+      this.nextSlide();
+    }, 10000);
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  previousSlide() {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  setSlide(index: number) {
+    this.currentSlide = index;
+  }
+  getSlideTransform(): string {
+    return `translateX(-${this.currentSlide * 100}%)`;
+  }
 }
